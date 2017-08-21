779fda2e5418143d6d9dd28847f10ac7d6fa7782
Dmitriy Romanenko <dmitriy@ascendify.com>
2017-08-21 13:42:19 -0700

    Added url example

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,4 @@
 import axios from 'axios';
 const API_KEY = '982967731cbfe4c44b240ef9a7c06065';
 const ROOT_URL =  `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+// url example `${ROOT_URL}&q=${city},us`;
